e8f611c374a11e6a948dea2b517837527e0eee7e
WikiRik <WikiRik@users.noreply.github.com>
2021-10-13 22:44:19 +0000

    ci(tests): re-enable other tests

--- test/accounts.test.js
+++ test/accounts.test.js
@@ -1,359 +1,359 @@
-// const chai = require('chai');
-// const should = chai.should();
-// const { request } = require('./test-helper.js');
-// const {runGsuiteOperation, gsuiteOperations} = require('../lib/google-suite.js');
-// const crypto = require('crypto');
-
-// const redis = require('../lib/redis.js').db;
-// function delay(interval){
-//   return it('should delay', done => { setTimeout(() => done(), interval); },
-//   ).timeout(interval + 100); // The extra 100ms should guarantee the test will not fail due to exceeded timeout
-// }
-
-// describe('Accounts', function(){
-
-//   const name = 'Automated';
-//   const surname = 'APITest';
-//   const generatedUsername = name.toLowerCase() + '.' + surname.toLowerCase() + '@aegee.eu';
-//   const email = 'alternatemail817263@mailinator.com';
-//   const antenna = 'AEGEE-Tallahassee';
-//   const password = 'AEGEE-Europe';
-//   const SHA1Password = crypto.createHash('sha1').update(JSON.stringify(password)).digest('hex');
-//   const userPK = 'totallyuuid-account';
-
-//   const data = {
-//     primaryEmail: generatedUsername,
-//     name: {
-//       givenName: name,
-//       familyName: surname,
-//     },
-//     secondaryEmail: email,
-//     password: SHA1Password,
-//     antenna: antenna,
-//     userPK: userPK,
-//   };
-
-//   after('Remove user', async function(){
-//     this.timeout(8000);
-
-//     let keys = await redis.keys('*');
-//     console.log(keys);
-
-//     let result = await runGsuiteOperation(gsuiteOperations.deleteAccount, data);
-//     console.log(result);
-
-//     const user_primaryEmail = generatedUsername;
-//     const user_secondaryEmail = email;
-
-//     pip = redis.pipeline();
-
-//     // user
-//     pip.hdel('user:' + userPK, 'GsuiteAccount');
-//     pip.hdel('user:' + userPK, 'SecondaryEmail');
-//     pip.del('primary:' + userPK, 'primary:' + user_secondaryEmail, 'id:' + user_primaryEmail, 'secondary:' + user_primaryEmail);
-//     pip.exec((err, res) => { console.log(err); console.log(res); });
+const chai = require('chai');
+const should = chai.should();
+const { request } = require('./test-helper.js');
+const {runGsuiteOperation, gsuiteOperations} = require('../lib/google-suite.js');
+const crypto = require('crypto');
+
+const redis = require('../lib/redis.js').db;
+function delay(interval){
+  return it('should delay', done => { setTimeout(() => done(), interval); },
+  ).timeout(interval + 100); // The extra 100ms should guarantee the test will not fail due to exceeded timeout
+}
+
+describe('Accounts', function(){
+
+  const name = 'Automated';
+  const surname = 'APITest';
+  const generatedUsername = name.toLowerCase() + '.' + surname.toLowerCase() + '@aegee.eu';
+  const email = 'alternatemail817263@mailinator.com';
+  const antenna = 'AEGEE-Tallahassee';
+  const password = 'AEGEE-Europe';
+  const SHA1Password = crypto.createHash('sha1').update(JSON.stringify(password)).digest('hex');
+  const userPK = 'totallyuuid-account';
+
+  const data = {
+    primaryEmail: generatedUsername,
+    name: {
+      givenName: name,
+      familyName: surname,
+    },
+    secondaryEmail: email,
+    password: SHA1Password,
+    antenna: antenna,
+    userPK: userPK,
+  };
+
+  after('Remove user', async function(){
+    this.timeout(8000);
+
+    let keys = await redis.keys('*');
+    console.log(keys);
+
+    let result = await runGsuiteOperation(gsuiteOperations.deleteAccount, data);
+    console.log(result);
+
+    const user_primaryEmail = generatedUsername;
+    const user_secondaryEmail = email;
+
+    pip = redis.pipeline();
+
+    // user
+    pip.hdel('user:' + userPK, 'GsuiteAccount');
+    pip.hdel('user:' + userPK, 'SecondaryEmail');
+    pip.del('primary:' + userPK, 'primary:' + user_secondaryEmail, 'id:' + user_primaryEmail, 'secondary:' + user_primaryEmail);
+    pip.exec((err, res) => { console.log(err); console.log(res); });
 
-//     keys = await redis.keys('*');
-//     console.log(keys);
-//   });
-
-//   describe('POST /account', function(){
-
-//     it('Should add an account if valid', async() => {
-//       this.timeout(3000);
-//       const payload = JSON.parse(JSON.stringify(data));
-
-//       const res = await request({
-//         uri: '/account',
-//         method: 'POST',
-//         headers: { 'test-title': 'create account' },
-//         body: payload,
-//       });
+    keys = await redis.keys('*');
+    console.log(keys);
+  });
+
+  describe('POST /account', function(){
+
+    it('Should add an account if valid', async() => {
+      this.timeout(3000);
+      const payload = JSON.parse(JSON.stringify(data));
+
+      const res = await request({
+        uri: '/account',
+        method: 'POST',
+        headers: { 'test-title': 'create account' },
+        body: payload,
+      });
 
-//       const body = res.body;
-//       res.statusCode.should.equal(201);
-//       body.success.should.equal(true);
+      const body = res.body;
+      res.statusCode.should.equal(201);
+      body.success.should.equal(true);
 
-//     });
-
-//     it('Should not add an account if already existing', async() => {
+    });
+
+    it('Should not add an account if already existing', async() => {
 
-//       const payload = JSON.parse(JSON.stringify(data));
-
-//       const res = await request({
-//         uri: '/account',
-//         method: 'POST',
-//         headers: { 'test-title': 'fail create account' },
-//         body: payload,
-//       });
+      const payload = JSON.parse(JSON.stringify(data));
+
+      const res = await request({
+        uri: '/account',
+        method: 'POST',
+        headers: { 'test-title': 'fail create account' },
+        body: payload,
+      });
 
-//       const body = res.body;
-//       res.statusCode.should.equal(409);
-//       body.success.should.equal(false);
+      const body = res.body;
+      res.statusCode.should.equal(409);
+      body.success.should.equal(false);
 
-//     });
+    });
 
-//     it('Should not add an account if without primaryEmail', async() => {
+    it('Should not add an account if without primaryEmail', async() => {
 
-//       const payload = JSON.parse(JSON.stringify(data));
-//       delete payload.primaryEmail;
+      const payload = JSON.parse(JSON.stringify(data));
+      delete payload.primaryEmail;
 
-//       const res = await request({
-//         uri: '/account',
-//         method: 'POST',
-//         headers: { 'test-title': 'fail create account' },
-//         body: payload,
-//       });
+      const res = await request({
+        uri: '/account',
+        method: 'POST',
+        headers: { 'test-title': 'fail create account' },
+        body: payload,
+      });
 
-//       const body = res.body;
-//       res.statusCode.should.equal(400);
-//       body.success.should.equal(false);
+      const body = res.body;
+      res.statusCode.should.equal(400);
+      body.success.should.equal(false);
 
-//     });
+    });
 
-//     it('Should not add an account if primaryEmail is empty', async() => {
+    it('Should not add an account if primaryEmail is empty', async() => {
 
-//       const payload = JSON.parse(JSON.stringify(data));
-//       payload.primaryEmail = '';
+      const payload = JSON.parse(JSON.stringify(data));
+      payload.primaryEmail = '';
 
-//       const res = await request({
-//         uri: '/account',
-//         method: 'POST',
-//         headers: { 'test-title': 'fail create account' },
-//         body: payload,
-//       });
+      const res = await request({
+        uri: '/account',
+        method: 'POST',
+        headers: { 'test-title': 'fail create account' },
+        body: payload,
+      });
 
-//       const body = res.body;
-//       res.statusCode.should.equal(400);
-//       body.success.should.equal(false);
+      const body = res.body;
+      res.statusCode.should.equal(400);
+      body.success.should.equal(false);
 
-//     });
+    });
 
-//     it('Should not add an account if without secondaryEmail', async() => {
+    it('Should not add an account if without secondaryEmail', async() => {
 
-//       const payload = JSON.parse(JSON.stringify(data));
-//       delete payload.secondaryEmail;
+      const payload = JSON.parse(JSON.stringify(data));
+      delete payload.secondaryEmail;
 
-//       const res = await request({
-//         uri: '/account',
-//         method: 'POST',
-//         headers: { 'test-title': 'fail create account' },
-//         body: payload,
-//       });
+      const res = await request({
+        uri: '/account',
+        method: 'POST',
+        headers: { 'test-title': 'fail create account' },
+        body: payload,
+      });
 
-//       const body = res.body;
-//       res.statusCode.should.equal(400);
-//       body.success.should.equal(false);
+      const body = res.body;
+      res.statusCode.should.equal(400);
+      body.success.should.equal(false);
 
-//     });
+    });
 
-//     it('Should not add an account if secondaryEmail is empty', async() => {
+    it('Should not add an account if secondaryEmail is empty', async() => {
 
-//       const payload = JSON.parse(JSON.stringify(data));
-//       payload.secondaryEmail = '';
+      const payload = JSON.parse(JSON.stringify(data));
+      payload.secondaryEmail = '';
 
-//       const res = await request({
-//         uri: '/account',
-//         method: 'POST',
-//         headers: { 'test-title': 'fail create account' },
-//         body: payload,
-//       });
+      const res = await request({
+        uri: '/account',
+        method: 'POST',
+        headers: { 'test-title': 'fail create account' },
+        body: payload,
+      });
 
-//       const body = res.body;
-//       res.statusCode.should.equal(400);
-//       body.success.should.equal(false);
+      const body = res.body;
+      res.statusCode.should.equal(400);
+      body.success.should.equal(false);
 
-//     });
+    });
 
-//     it('Should not add an account if without password', async() => {
+    it('Should not add an account if without password', async() => {
 
-//       const payload = JSON.parse(JSON.stringify(data));
-//       delete payload.password;
+      const payload = JSON.parse(JSON.stringify(data));
+      delete payload.password;
 
-//       const res = await request({
-//         uri: '/account',
-//         method: 'POST',
-//         headers: { 'test-title': 'fail create account' },
-//         body: payload,
-//       });
+      const res = await request({
+        uri: '/account',
+        method: 'POST',
+        headers: { 'test-title': 'fail create account' },
+        body: payload,
+      });
 
-//       const body = res.body;
-//       res.statusCode.should.equal(400);
-//       body.success.should.equal(false);
+      const body = res.body;
+      res.statusCode.should.equal(400);
+      body.success.should.equal(false);
 
-//     });
+    });
 
-//     it('Should not add an account if password is empty', async() => {
+    it('Should not add an account if password is empty', async() => {
 
-//       const payload = JSON.parse(JSON.stringify(data));
-//       payload.password = '';
+      const payload = JSON.parse(JSON.stringify(data));
+      payload.password = '';
 
-//       const res = await request({
-//         uri: '/account',
-//         method: 'POST',
-//         headers: { 'test-title': 'fail create account' },
-//         body: payload,
-//       });
+      const res = await request({
+        uri: '/account',
+        method: 'POST',
+        headers: { 'test-title': 'fail create account' },
+        body: payload,
+      });
 
-//       const body = res.body;
-//       res.statusCode.should.equal(400);
-//       body.success.should.equal(false);
+      const body = res.body;
+      res.statusCode.should.equal(400);
+      body.success.should.equal(false);
 
-//     });
+    });
 
-//     it('Should not add an account if without antenna', async() => {
+    it('Should not add an account if without antenna', async() => {
 
-//       const payload = JSON.parse(JSON.stringify(data));
-//       delete payload.antenna;
+      const payload = JSON.parse(JSON.stringify(data));
+      delete payload.antenna;
 
-//       const res = await request({
-//         uri: '/account',
-//         method: 'POST',
-//         headers: { 'test-title': 'fail create account' },
-//         body: payload,
-//       });
+      const res = await request({
+        uri: '/account',
+        method: 'POST',
+        headers: { 'test-title': 'fail create account' },
+        body: payload,
+      });
 
-//       const body = res.body;
-//       res.statusCode.should.equal(400);
-//       body.success.should.equal(false);
+      const body = res.body;
+      res.statusCode.should.equal(400);
+      body.success.should.equal(false);
 
-//     });
+    });
 
-//     it('Should not add an account if antenna is empty', async() => {
+    it('Should not add an account if antenna is empty', async() => {
 
-//       const payload = JSON.parse(JSON.stringify(data));
-//       payload.antenna = '';
+      const payload = JSON.parse(JSON.stringify(data));
+      payload.antenna = '';
 
-//       const res = await request({
-//         uri: '/account',
-//         method: 'POST',
-//         headers: { 'test-title': 'fail create account' },
-//         body: payload,
-//       });
+      const res = await request({
+        uri: '/account',
+        method: 'POST',
+        headers: { 'test-title': 'fail create account' },
+        body: payload,
+      });
 
-//       const body = res.body;
-//       res.statusCode.should.equal(400);
-//       body.success.should.equal(false);
+      const body = res.body;
+      res.statusCode.should.equal(400);
+      body.success.should.equal(false);
 
-//     });
+    });
 
-//     it('Should not add an account if without name', async() => {
+    it('Should not add an account if without name', async() => {
 
-//       const payload = JSON.parse(JSON.stringify(data));
-//       delete payload.name.givenName;
+      const payload = JSON.parse(JSON.stringify(data));
+      delete payload.name.givenName;
 
-//       const res = await request({
-//         uri: '/account',
-//         method: 'POST',
-//         headers: { 'test-title': 'fail create account' },
-//         body: payload,
-//       });
+      const res = await request({
+        uri: '/account',
+        method: 'POST',
+        headers: { 'test-title': 'fail create account' },
+        body: payload,
+      });
 
-//       const body = res.body;
-//       res.statusCode.should.equal(400);
-//       body.success.should.equal(false);
+      const body = res.body;
+      res.statusCode.should.equal(400);
+      body.success.should.equal(false);
 
-//     });
+    });
 
-//     it('Should not add an account if name is empty', async() => {
+    it('Should not add an account if name is empty', async() => {
 
-//       const payload = JSON.parse(JSON.stringify(data));
-//       payload.name.givenName = '';
+      const payload = JSON.parse(JSON.stringify(data));
+      payload.name.givenName = '';
 
-//       const res = await request({
-//         uri: '/account',
-//         method: 'POST',
-//         headers: { 'test-title': 'fail create account' },
-//         body: payload,
-//       });
+      const res = await request({
+        uri: '/account',
+        method: 'POST',
+        headers: { 'test-title': 'fail create account' },
+        body: payload,
+      });
 
-//       const body = res.body;
-//       res.statusCode.should.equal(400);
-//       body.success.should.equal(false);
+      const body = res.body;
+      res.statusCode.should.equal(400);
+      body.success.should.equal(false);
 
-//     });
+    });
 
-//     it('Should not add an account if without surname', async() => {
+    it('Should not add an account if without surname', async() => {
 
-//       const payload = JSON.parse(JSON.stringify(data));
-//       delete payload.name.familyName;
+      const payload = JSON.parse(JSON.stringify(data));
+      delete payload.name.familyName;
 
-//       const res = await request({
-//         uri: '/account',
-//         method: 'POST',
-//         headers: { 'test-title': 'fail create account' },
-//         body: payload,
-//       });
+      const res = await request({
+        uri: '/account',
+        method: 'POST',
+        headers: { 'test-title': 'fail create account' },
+        body: payload,
+      });
 
-//       const body = res.body;
-//       res.statusCode.should.equal(400);
-//       body.success.should.equal(false);
+      const body = res.body;
+      res.statusCode.should.equal(400);
+      body.success.should.equal(false);
 
-//     });
+    });
 
-//     it('Should not add an account if surname is empty', async() => {
+    it('Should not add an account if surname is empty', async() => {
 
-//       const payload = JSON.parse(JSON.stringify(data));
-//       payload.name.familyName = '';
+      const payload = JSON.parse(JSON.stringify(data));
+      payload.name.familyName = '';
 
-//       const res = await request({
-//         uri: '/account',
-//         method: 'POST',
-//         headers: { 'test-title': 'fail create account' },
-//         body: payload,
-//       });
+      const res = await request({
+        uri: '/account',
+        method: 'POST',
+        headers: { 'test-title': 'fail create account' },
+        body: payload,
+      });
 
-//       const body = res.body;
-//       res.statusCode.should.equal(400);
-//       body.success.should.equal(false);
+      const body = res.body;
+      res.statusCode.should.equal(400);
+      body.success.should.equal(false);
 
-//     });
+    });
 
-//     it('Should not add an account if without userPK', async() => {
+    it('Should not add an account if without userPK', async() => {
 
-//       const payload = JSON.parse(JSON.stringify(data));
-//       delete payload.userPK;
+      const payload = JSON.parse(JSON.stringify(data));
+      delete payload.userPK;
 
-//       const res = await request({
-//         uri: '/account',
-//         method: 'POST',
-//         headers: { 'test-title': 'fail create account' },
-//         body: payload,
-//       });
+      const res = await request({
+        uri: '/account',
+        method: 'POST',
+        headers: { 'test-title': 'fail create account' },
+        body: payload,
+      });
 
-//       const body = res.body;
-//       res.statusCode.should.equal(400);
-//       body.success.should.equal(false);
+      const body = res.body;
+      res.statusCode.should.equal(400);
+      body.success.should.equal(false);
 
-//     });
+    });
 
-//     it('Should not add an account if userPK is empty', async() => {
+    it('Should not add an account if userPK is empty', async() => {
 
-//       const payload = JSON.parse(JSON.stringify(data));
-//       payload.userPK = '';
+      const payload = JSON.parse(JSON.stringify(data));
+      payload.userPK = '';
 
-//       const res = await request({
-//         uri: '/account',
-//         method: 'POST',
-//         headers: { 'test-title': 'fail create account' },
-//         body: payload,
-//       });
+      const res = await request({
+        uri: '/account',
+        method: 'POST',
+        headers: { 'test-title': 'fail create account' },
+        body: payload,
+      });
 
-//       const body = res.body;
-//       res.statusCode.should.equal(400);
-//       body.success.should.equal(false);
+      const body = res.body;
+      res.statusCode.should.equal(400);
+      body.success.should.equal(false);
 
-//     });
+    });
 
-//   });
+  });
 
-//   delay(2000);
+  delay(2000);
 
-//   xdescribe('GET /account', function(){
+  xdescribe('GET /account', function(){
 
-//     it('Should list all accounts if valid', async() => {
+    it('Should list all accounts if valid', async() => {
 
-//     });
+    });
 
-//   });
-// });
+  });
+});
